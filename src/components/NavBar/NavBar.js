c959b0eb8b5d156bc62c7a7d3fdeff67f50646cb
Guido Gambini <guidogambini@hotmail.com>
2023-04-01 11:52:19 +0200

    rename navbar component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { FaTimes } from "react-icons/fa";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { FaTimes } from "react-icons/fa";
 import styles from "./Navbar.module.css";
 
 const Navbar = ({ logo, menu }) => {
